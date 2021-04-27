17104f655e8f8fcb6fc2336e098bb20e6c4013a5
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-04-26 22:46:14 -0300

    AJustando tela de inicio do jogo

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,117 @@
 <template>
-  <h1>Cover your page.</h1>
+  <h1>Olá</h1>
   <p class="lead">
-    Cover is a one-page template for building simple and beautiful home pages.
-    Download, edit the text, and add your own fullscreen background photo to
-    make it your own.
+    Clique na opção desejada para os tipos de jogadores que irão jogar e tempo
+    de turno
   </p>
-  <p class="lead">
-    <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-white"
-      >Learn more</a
-    >
-  </p>
+
+  <div class="mt-3">
+    <div class="btn-group" role="group">
+      <template v-for="shiftTime in shiftTimes" v-bind:key="shiftTime.id">
+        <input
+          type="radio"
+          class="btn-check"
+          :id="'btnShiftTime' + shiftTime.id"
+          :checked="shiftTime.ms == shiftTimeChoosed"
+          @change="setShiftTime(shiftTime.ms)"
+        />
+        <label
+          class="btn btn-outline-primary"
+          :for="'btnShiftTime' + shiftTime.id"
+          >{{ shiftTime.txt }}</label
+        >
+      </template>
+    </div>
+  </div>
+
+  <div class="d-flex">
+    <div class="m-auto w-100 row">
+      <div class="col-md-4 mt-3" v-for="type in types" v-bind:key="type.id">
+        <button
+          @click="createGameWithType(type.id)"
+          class="btn btn-lg btn-light fw-bold border-white w-100"
+        >
+          <span
+            v-bind:key="index"
+            v-for="(todosintegranteTipo, index) in type.integranteLadoTipo"
+          >
+            {{ (index != 0 ? " X " : "") + todosintegranteTipo.nome }}
+          </span>
+        </button>
+      </div>
+    </div>
+  </div>
+
+  <hr />
+  <h2>OU</h2>
+  <button class="btn btn-lg btn-light fw-bold border-white mt-3">
+    Entre em uma sala
+  </button>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, onMounted, ref } from "vue";
+import { useRouter } from "vue-router";
+import api from "../api";
 export default defineComponent({
   name: "Home",
+  setup: () => {
+    const router = useRouter();
+    let types = ref([]);
+    const shiftTimeChoosed = ref(-1);
+
+    const setShiftTime = (shiftTime: number) => {
+      shiftTimeChoosed.value = shiftTime;
+    };
+
+    const shiftTimes = [
+      { id: 0, ms: -1, txt: "Tempo Infinito" },
+      { id: 1, ms: 300000, txt: "5 Minutos" },
+      { id: 2, ms: 600000, txt: "10 Minutos" },
+      { id: 3, ms: 900000, txt: "15 Minutos" },
+    ];
+
+    const getGameTypes = async () => {
+      return await api
+        .get(`/tipos-de-jogo`)
+        .then((res) => {
+          return res.data.data;
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+          return [];
+        });
+    };
+
+    const createGameWithType = async (typeId: number) => {
+      return await api
+        .post(`/jogos`, {
+          tipoJogo: typeId,
+          tempoDeTurnoEmMilisegundos: shiftTimeChoosed.value,
+        })
+        .then((res) => {
+          router.push({ name: "game", params: { id: res.data.data.id } });
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+          return null;
+        });
+    };
+
+    const populateGameTypes = async () => {
+      types.value = await getGameTypes();
+    };
+
+    onMounted(async () => {
+      await populateGameTypes();
+      setShiftTime(-1);
+    });
+    return {
+      types,
+      createGameWithType,
+      shiftTimes,
+      shiftTimeChoosed,
+      setShiftTime,
+    };
+  },
 });
 </script>
